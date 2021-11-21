e0480ef18a66316c77d6e1d11c5558bf42424c9f
Hrishikesh Mahajan <mahajanhm4@gmail.com>
2021-11-21 11:58:27 -0500

    Create generate.php

---
+++ generate.php
@@ -0,0 +1,25 @@
+<?php
+	echo($_GET['message']);
+?>
+<html>
+<head>
+  <link rel="stylesheet" href="style.css">
+</head>
+<script src="script.js"></script>
+
+<body style="height:100%">
+
+<header><h2>QUIZ</h2>
+</header>
+<div style="height:100%" id="form">
+
+<div class="fish" id="fish"></div>
+<div class="fish" id="fish2"></div>
+<center>
+<div>
+
+</div>
+<center>
+
+</div>
+</body>
